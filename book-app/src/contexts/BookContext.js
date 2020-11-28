776b98df6486fe9e4ab1f43c5e618a0e3eb275ab
Supuni22 <supunimanamperi@gmail.com>
2020-11-28 19:20:46 +0530

    Modified book context with add and remove functions

--- book-app/src/contexts/BookContext.js
+++ book-app/src/contexts/BookContext.js
@@ -1,17 +1,31 @@
-import React, {useState, createContext} from 'react'
+import React, {useState, createContext, useCallback} from 'react'
 
 export const BookContext = createContext()
 
 const BookContextProvider = ({children}) => {
 
     const [books, setBooks] = useState([
-        {name :'The way of kings' , id : 1},
-        {name :'Twilight' , id : 2},
-        {name :'Harry Potter' , id : 3}
+        {name :'The way of kings' , author: 'Patrick Thomson', id : 1},
+        {name :'Twilight' ,author: 'Stephony Meyor', id : 2},
+        {name :'Harry Potter', author: 'J.K Rowling', id : 3}
     ])
 
+    const addBook = useCallback(
+        (bookName, author) => {
+            setBooks([...books, {name:bookName, author:author, id :  Math.random()*100}])
+        },
+        [setBooks,books]
+    );
+
+    const removeBook = useCallback(
+        (id) => {
+            setBooks(books.filter(book => book.id !== id));
+        },
+        [setBooks,books]
+    );
+
     return (
-       <BookContext.Provider value={{books, setBooks}}>
+       <BookContext.Provider value={{books, addBook, removeBook}}>
            {children}
        </BookContext.Provider>
     )
